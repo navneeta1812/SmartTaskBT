5b64e9900067d6baa5a54b38ba49cd6e06f4891c
Navneeta <navneetaprasad1812@gmail.com>
2021-04-13 02:01:04 +0530

    new commit

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 export default class loginPage extends React.Component {
 
   constructor(props){
+    
     super(props);
      this.state = {
        isDisabled:true
