95f85019394c8a393c92e1eb59c216c4eba6e2a9
Navneeta <navneetaprasad1812@gmail.com>
2021-05-03 13:41:51 +0530

    npm

--- src/components/viewTask/myVTmain/MyVTmain.js
+++ src/components/viewTask/myVTmain/MyVTmain.js
@@ -14,34 +14,33 @@ export default class VTmain extends React.Component {
     }
 
     componentDidMount(){
-
         this.getTaskReport();
     }
 
     getTaskReport(){
 
-        fbHelper.database().ref("tasks").on("value",snap=>{
-            let newTaskState=[];
-            snap.forEach(data => {
-                const task = data.val()
-                newTaskState.push({
-                  title: task.title,
-                  description: task.description,
-                  assingedTo:task.assingedTo,
-                  created:task.created_At,
-                  url:task.imageUrl,
-                  project:task.project,
-                  priority: task.priority,
-                  assinedID:task.assinedID,
-                  submittedBy:task.editor_ID
-                  
-                })
-            })
+      fbHelper.database().ref("tasks").on("value",snap=>{
+          let newTaskState=[]; 
+          snap.forEach(data => {
+              const task = data.val()
+              newTaskState.push({
+                title: task.title,
+                description: task.description,
+                assingedTo:task.assingedTo,
+                created:task.created_At,
+                url:task.imageUrl,
+                project:task.project,
+                priority: task.priority,
+                assinedID:task.assinedID,
+                submittedBy:task.editor_ID
+                
+              })
+          })
 
-            this.setState({
-                taskList:newTaskState
-            })
-        })
+          this.setState({
+              taskList:newTaskState
+          })
+      })
 
     }
 
@@ -49,20 +48,18 @@ export default class VTmain extends React.Component {
     render(){
         return (
           <div className="main_div">
-            {this.state.taskList.length > 0 ?
-            this.state.taskList.map((task) =>
-                <div className="task_card">
-                  <h4>{task.title}</h4>
-                  <h5>{task.description}</h5>
-                  {
-                     task.url.length>1?<img className="task_img" width="150" height = "100" src={task.url}></img>:''
-                  }
-                  <p>Created at {task.created}</p>
-                  <p>Project : {task.project}</p>
-                  <p>Assinged to {task.assingedTo}</p>
-                  <p>Priority : {task.priority}</p>
-                  {/* <p>submitted by {task.submittedBy}</p> */}
-    
+            {this.state.taskList.length > 0 ?  this.state.taskList.map((task) =>
+              <div className="task_card">
+                <h4>{task.title}</h4>
+                <h5>{task.description}</h5>
+                {
+                  task.url.length > 1 ? <img className="task_img" width="150" height = "100" src={task.url}></img> : ''
+                }
+                <p>Created at {task.created}</p>
+                <p>Project : {task.project}</p>
+                <p>Assinged to {task.assingedTo}</p>
+                <p>Priority : {task.priority}</p>
+                {/* <p>submitted by {task.submittedBy}</p> */}
               </div>
             )  : 
               <h3>NO  TASK  YET</h3>
